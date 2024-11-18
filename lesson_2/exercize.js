14107a6c7edff545cf4c7e3929362698b6e96c52
Kanagat Bekmaganbetov <bekkananur@gmail.com>
2024-11-18 11:32:12 +0500

    initial commit

---
+++ lesson_2/exercize.js
@@ -0,0 +1,4 @@
+//1 задание
+const isLoggedIn = true;
+const strIsLoggenIn = "" + isLoggedIn;
+console.log(strIsLoggedIn, typeOf, strIsLoggenIn);
---
+++ lesson_2/script.js
@@ -0,0 +1,3 @@
+const year = 1995;
+console.log(year);
+console.log(String());
